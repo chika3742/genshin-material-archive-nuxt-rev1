024931b6b9aec928284f91fdde70a6ca17baa936
chika3742 <kazu.chika.shima@gmail.com>
2023-03-19 00:53:08 +0900

    indexページに文言の追加

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div />
+  <div>
+    <h1 class="font-kaisei-opti mb-4">
+      {{ $t("common.appName") }}
+    </h1>
+    <p>原神プレイヤーのための便利ツール群</p>
+  </div>
 </template>
 
 <script lang="ts" setup>
