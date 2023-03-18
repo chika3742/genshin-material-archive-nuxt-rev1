607d63f483259e378c1b069bfe40529cd8b224af
chika3742 <kazu.chika.shima@gmail.com>
2023-03-18 23:30:56 +0900

    init i18n/app.vue

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -13,4 +13,34 @@ export default defineNuxtConfig({
   build: {
     transpile: ["vuetify"],
   },
+  modules: [
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    locales: [
+      {
+        code: "ja",
+        iso: "ja-JP",
+        file: "ja.yaml",
+      },
+      {
+        code: "en",
+        iso: "en-US",
+        file: "en.yaml",
+      },
+    ],
+    langDir: "./locales/",
+    defaultLocale: "ja",
+    vueI18n: {
+      legacy: false,
+      fallbackLocale: "ja",
+      datetimeFormats: {
+        ja: {
+          time: {
+            hour12: false,
+          },
+        },
+      },
+    },
+  },
 })
