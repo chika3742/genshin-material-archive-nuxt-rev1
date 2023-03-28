a62caeeba7ab2d6dfbca41c3dd392a53a3c8e18b
chika3742 <kazu.chika.shima@gmail.com>
2023-03-28 16:44:25 +0900

    feat: エラーページ追加

---
+++ nuxt/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ma-4">
+    <h1>{{ error.statusCode }} {{ error.statusMessage }}</h1>
+    <p>{{ error.message }}</p>
+    <v-btn class="mt-4" @click="handleError">
+      {{ $t("common.backToHome") }}
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {NuxtError} from "#app"
+
+defineProps<{
+  error: NuxtError
+}>()
+
+const localePath = useLocalePath()
+
+const handleError = () => {
+  clearError({redirect: localePath("/")})
+}
+</script>
