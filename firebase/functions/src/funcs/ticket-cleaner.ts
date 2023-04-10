a8ddb3eb5ac317fda7a50237e816ee20ec495e6a
chika3742 <kazu.chika.shima@gmail.com>
2023-04-10 23:45:08 +0900

    fix: ticketCleanerのリージョン修正

--- firebase/functions/src/funcs/ticket-cleaner.ts
+++ firebase/functions/src/funcs/ticket-cleaner.ts
@@ -3,7 +3,7 @@ import {firestoreCollections} from "../lib/firestore-collections.js"
 
 const expiresIn = 1000 * 60 * 30 // 30 minutes
 
-export const ticketCleaner = functions.pubsub
+export const ticketCleaner = functions.region("asia-northeast1").pubsub
   .schedule("every 1 hours synchronized")
   .timeZone("Asia/Tokyo")
   .onRun(async(context) => {
