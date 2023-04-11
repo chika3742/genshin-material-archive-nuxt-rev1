72ac6cb5011106d432f8ce5e9482c59855510dda
chika3742 <kazu.chika.shima@gmail.com>
2023-04-11 23:02:16 +0900

    祈願履歴取得をCloud Tasksを使用した実装に変更

--- firebase/functions/src/funcs/get-wish-history.ts
+++ firebase/functions/src/funcs/get-wish-history.ts
@@ -1,10 +1,16 @@
 import functions from "firebase-functions"
-import {DispatchGetWishHistoryParams} from "../types/shared/dispatch-get-wish-history.js"
+import {GachaLogRequest} from "../lib/gacha-log-request.js"
+import {GetWishHistoryParams} from "../types/get-wish-history-params"
+import {firestoreCollections} from "../lib/firestore-collections.js"
 
 export const getWishHistory = functions.region("asia-northeast1").tasks.taskQueue({
   rateLimits: {
     maxConcurrentDispatches: 5,
   },
-}).onDispatch((data: DispatchGetWishHistoryParams) => {
-  console.log(data)
+}).onDispatch((data: GetWishHistoryParams) => {
+  return new GachaLogRequest(data.authKey, data.region, (processedCount) => {
+    return firestoreCollections.wishHistoryTickets.doc(data.ticketId).update({
+      count: processedCount,
+    })
+  }).getGachaLogForAllWishTypes(data.lastIds).then()
 })
