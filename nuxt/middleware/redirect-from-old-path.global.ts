aadba01e07bd150729751a7ef37fbfcb7fb43263
chika3742 <kazu.chika.shima@gmail.com>
2023-04-17 14:46:02 +0900

    リダイレクト設定

--- nuxt/middleware/redirect-from-old-path.global.ts
+++ nuxt/middleware/redirect-from-old-path.global.ts
@@ -1,8 +1,28 @@
 export default defineNuxtRouteMiddleware((to) => {
   // /character/** -> /characters/**
-  const match = to.path.match(/^\/character(?:\/.*)?$/)
+  {
+    const match = to.path.match(/^\/character(?:\/.*)?$/)
 
-  if (match) {
-    return navigateTo(`/characters${match[1] || ""}`, {redirectCode: 308})
+    if (match) {
+      return navigateTo(`/characters${match[1] || ""}`, {redirectCode: 308})
+    }
+  }
+
+  // /weapon/** -> /weapons/**
+  {
+    const match = to.path.match(/^\/weapon(?:\/.*)?$/)
+
+    if (match) {
+      return navigateTo(`/weapons${match[1] || ""}`, {redirectCode: 308})
+    }
+  }
+
+  // /material/** -> /materials/**
+  {
+    const match = to.path.match(/^\/material(?:\/.*)?$/)
+
+    if (match) {
+      return navigateTo(`/materials${match[1] || ""}`, {redirectCode: 308})
+    }
   }
 })
